2ce2814b0fe5f26bfbafcafbf89bc7d914b52a5a
Ilyas <illatiph@gmail.com>
2017-09-07 17:30:08 +0500

    Fixed insert errors

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -6,26 +6,31 @@ typedef struct list {
 } list;
 
 list *search_list(list*, int);
-void insert_list(list**, int);
+void insert_list(list*, int);
 
 int main(void) {
     printf("Start...\n");
 
-    insert_list(&&num, 3);
+    list data;
+
+    insert_list(&data, 3);
+
+    printf("%d", data.item);
 
     return 0;
 }
 
 /**
  * Insert value to linked list.
+ * @todo Inserting the first element works, but next inserts overwrite the first element.
  **/
-void insert_list(list **list_imp, int x) {
+void insert_list(list *list_imp, int x) {
     list* tempPointer;
 
     tempPointer = malloc(sizeof(list));
     tempPointer->item = x;
-    tempPointer->next = *list_imp;
-    *list_imp = tempPointer;
+    tempPointer->next = &list_imp;
+    *list_imp = *tempPointer;
 }
 
 /**
