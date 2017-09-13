34816c774393c9dd31f12835ee580d1a4fd0bd0c
Ilyas <illatiph@gmail.com>
2017-09-13 11:35:36 +0500

    В процессе реализация бинарного дерева поиска

--- binary_search_tree/binary_search_tree.c
+++ binary_search_tree/binary_search_tree.c
@@ -2,13 +2,31 @@
 
 typedef struct binary_tree {
     int item;
-    binary_tree *parent;
-    binary_tree *left;
-    binary_tree *right;
+    struct binary_tree *parent;
+    struct binary_tree *left;
+    struct binary_tree *right;
 } binary_tree;
+
+binary_tree* create_node(binary_tree*, int);
 
 int main(void) {
     printf("Start...");
 
+    // Инициализируем корневой элемент дерева
+    binary_tree tree;
+
+    //tree.item = 3;
+    //tree.parent = NULL;
+    //tree.left = NULL;
+    //tree.right = NULL;
+
+    create_node(&tree, 5);
+
     return 0;
+}
+
+binary_tree* create_node(binary_tree* parent, int x) {
+    printf("%d", parent);
+
+    return parent;
 }
